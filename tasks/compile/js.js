1ab68a43467540c3bab0f7a78b9fa04882e19764
Vlad Filip <vlad.filip@williamhill.com>
2016-11-16 15:36:25 +0100

    added uglify to js

--- tasks/compile/js.js
+++ tasks/compile/js.js
@@ -6,6 +6,7 @@ const mkdir = require( __dirname + '/../../utils/mkdir' )
 const fs = require( 'fs' )
 const browserify = require( 'browserify' )
 const babelify = require( 'babelify' )
+const uglify = require( 'uglify-js' )
 
 let input = './src/scripts/index.js'
 let output = './build/scripts/app.js'
@@ -15,7 +16,18 @@ sprockets.add( 'js', ( done ) => {
   browserify( input, { debug: true } )
         .transform( babelify )
         .bundle()
-        .on( 'error', function ( err ) { this.emit( 'end' ) } )
+        .on( 'error', function ( ) { this.emit( 'end' ) } )
         .on( 'end', done )
-        .pipe( fs.createWriteStream( output ) )
+        .pipe( function ( stream ) {
+          const chunks = []
+          stream.on( 'data', ( chunk ) => {
+            chunks.push( chunk.toString() )
+          } )
+          stream.on( 'end', () => {
+            var code = uglify.parse( chunks.join( '' ) )
+            fs.writeFile( output, code, function ( err ) {
+              done( err )
+            } )
+          } )
+        } )
 } )
