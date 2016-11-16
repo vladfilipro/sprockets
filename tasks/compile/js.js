27fe744f567cad850b5d576d7c0393ffadf0436f
Vlad Filip <vlad.filip@williamhill.com>
2016-11-16 16:13:21 +0100

    fixed stream uglifier

--- tasks/compile/js.js
+++ tasks/compile/js.js
@@ -13,21 +13,18 @@ let output = './build/scripts/app.js'
 
 sprockets.add( 'js', ( done ) => {
   mkdir( output )
+  const chunks = []
   browserify( input, { debug: true } )
         .transform( babelify )
         .bundle()
         .on( 'error', function ( ) { this.emit( 'end' ) } )
-        .on( 'end', done )
-        .pipe( function ( stream ) {
-          const chunks = []
-          stream.on( 'data', ( chunk ) => {
-            chunks.push( chunk.toString() )
-          } )
-          stream.on( 'end', () => {
-            var code = uglify.parse( chunks.join( '' ) )
-            fs.writeFile( output, code, function ( err ) {
-              done( err )
-            } )
+        .on( 'data', ( chunk ) => {
+          chunks.push( chunk.toString() )
+        } )
+        .on( 'end', function () {
+          var code = uglify.minify( chunks.join( '' ), { fromString: true } ).code
+          fs.writeFile( output, code, function ( err ) {
+            done( err )
           } )
         } )
 } )
