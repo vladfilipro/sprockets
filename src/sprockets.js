836e1c7b4df867352c75ad36a2485d240fb648b7
Vlad Filip <vlad.filip@williamhill.com>
2016-11-16 10:11:10 +0100

    concept of built-in tasks

---
+++ src/sprockets.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const Task = require( __dirname + '/task' )
+
+let tasks = {}
+
+let add = ( id, f ) => {
+  if ( tasks[ f ] !== undefined ) {
+    throw new Error( 'A task with the id ' + id + ' has already been defined' )
+  }
+  tasks[ id ] = new Task( id, f )
+  return methods
+}
+
+let remove = ( id ) => {
+  delete tasks[ id ]
+  return methods
+}
+
+let get = ( id ) => {
+  if ( !tasks[ id ] ) {
+    throw new Error( 'Task ' + id + ' does not exist.' )
+  }
+  return tasks[ id ]
+}
+
+let getTaskPromise = ( task ) => {
+  return ( typeof task === 'string' ) ? get( task ).execute() : task.execute()
+}
+
+let runSync = function () { // cannot use arguments if we use arrow function
+  let list = [ ...arguments ].map( getTaskPromise )
+  let resolveSync = ( promises, resolve, reject ) => {
+    let promise = promises.splice( 0, 1 )
+    promise.then( () => {
+      if ( promises.length > 0 ) {
+        resolveSync( promises, resolve, reject )
+      } else {
+        resolve()
+      }
+    } ).catch( () => {
+      reject()
+    } )
+  }
+  return new Promise( ( resolve, reject ) => {
+    resolveSync( list, resolve, reject )
+  } )
+}
+
+let runAsync = function () { // cannot use arguments if we use arrow function
+  let list = [ ...arguments ].map( getTaskPromise )
+  return Promise.all( list )
+}
+
+let methods = {
+  add: add,
+  remove: remove,
+  get: get,
+  runSync: runSync,
+  run: runAsync
+}
+
+module.exports = methods
---
+++ tasks/compile/index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const sprockets = require( __dirname + '/../../src/' )
+
+const webpack = require( 'webpack' )
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' )
+
+var defaults = {
+  entry: {
+    app: './src/scripts/index.js'
+  },
+  output: {
+    filename: 'scripts/app.js',
+    path: './build'
+  },
+  resolve: {
+    extensions: [ '', '.js', '.json' ]
+  },
+  plugins: [
+    new CopyWebpackPlugin( [ { from: './src/index.html', to: 'index.html' } ], { copyUnmodified: true } ),
+    new ExtractTextPlugin( './src/styles/app.css', { allChunks: true } )
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: new ExtractTextPlugin( 'styles/app.css' ).extract( [ 'css', 'sass' ] )
+      }
+    ]
+  }
+}
+
+sprockets.add( 'build', ( done ) => {
+  let compiler = webpack( defaults )
+  compiler.run( done )
+} )
