de1de34f4f2f4ae35ac3f41d0bd031154c0cc802
Vlad <vlad.filip.dev@gmail.com>
2016-11-17 23:17:56 +0100

    started on routes

---
+++ tasks/webserver/routes/index.js
@@ -0,0 +1,34 @@
+
+var fs = require( 'fs' )
+var path = require( 'path' )
+
+var types = {
+  'js': 'application/javascript',
+  'css': 'text/css',
+  'png': 'image/png',
+  'jpg': 'image/jpeg',
+  'mp3': 'audio/mpeg'
+}
+
+var getContentType = function ( file ) {
+  var extension = path.extname( file ).replace( '.', '' )
+  return types[ extension ] || 'text/plain'
+}
+
+module.exports = function () {
+  var ErrorClass = this.Error
+  var fail = new ErrorClass( 404 )
+
+  this.execute = function ( req, cb, ecb ) {
+    try {
+      var file = './build' + req.query.path
+      var content = fs.readFileSync( file )
+      cb( content, 200, {
+        'Content-type': getContentType( file ),
+        'Cache-Control': 'max-age=604800, public'
+      } )
+    } catch ( e ) {
+      ecb( fail )
+    }
+  }
+}
---
+++ tasks/webserver/static.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const sprockets = require( __dirname + '/../../src/sprockets' )
+const Apigeon = require( 'apigeon' )
+
+sprockets.add( 'webserver-dev', ( done ) => {
+  var regexFile = new RegExp( '^(.*)$', 'i' )
+
+  // Define server
+  var apigeon = new Apigeon( {
+    paths: {
+      routes: __dirname + '/routes'
+    },
+    rewrite: function ( url ) {
+      var results
+
+      results = url.match( regexFile )
+      if ( results ) {
+        var file = ( results[ 1 ] === '' || results[ 1 ] === '/' ) ? 'index.html' : results[ 1 ]
+        return '/?path=' + encodeURIComponent( file )
+      }
+
+      return url
+    }
+  } )
+
+  apigeon.enableREST()
+
+  return apigeon
+} )
